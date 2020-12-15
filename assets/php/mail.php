bf0671112c266238797732d8d28897d1439b7d06
KMinetto <killian.minetto@gmail.com>
2020-12-15 14:48:51 +0100

    changes

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -29,7 +29,7 @@ http://localhost/ChristmasCard/christmasCard/wishCard.php?fname=". $_POST['first
     $title = htmlspecialchars($_POST['title']);
     $content = htmlspecialchars("{$_POST['message']}, voici le lien de la carte :
 https://kilianm.promo-45.codeur.online/christmasCard/wishCard.php?fname=". $_POST['firstName'] . "&amp;lname=" . $_POST['lastName'] .
-        "&amp;titleContent=" . $_POST['titleContent'] . "&amp;messageContent=" . $_POST['messageContent'], 'text/html');
+        "&amp;titleContent=" . $_POST['titleContent'] . "&amp;messageContent=" . $_POST['messageContent']);
     $headers = "From: {$email1}" . "\r\n" .
         "Reply-To: {$email2}" . "\r\n" .
         'X-Mailer: PHP/' . PHPVERSION();
